fac8720df8c5edd58be8e3980d3bd1b27b15bfab
Jordy51 <adityakumawat51@gmail.com>
2021-01-30 22:15:28 +0530

    ips  working

--- server.js
+++ server.js
@@ -13,11 +13,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 let ips = Array();
-console.log(ips);
+
 app.use((req, res, next) => {
 	const clientIp = requestIp.getClientIp(req);
-	ips.push([clientIp, Date.now()]);
-	console.log("Inside " + ips);
+	let i = 0;
+	let flag = 0;
+	for (i = 0; i < ips.length; i = i + 1) {
+		if (ips[i].ip == clientIp) {
+			ips[i].count = ips[i].count + 1;
+			if (ips[i].count % 10 == 0) {
+				if (ips[i].time + 10 > Math.floor(Date.now() / 1000)) {
+					console.log("Alert TOoo may request by IP " + ips[i].ip);
+				}
+				ips[i].time = Math.floor(Date.now() / 1000);
+			}
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		ips.push({ ip: clientIp, time: Math.floor(Date.now() / 1000), count: 0 });
+	}
+	console.log(ips);
 	next();
 });
 
