eb3151d86ee6bb650c2dfbc3354d319fc3e3f90f
Jordy51 <adityakumawat51@gmail.com>
2021-01-31 13:35:40 +0530

    Method Bug removed and Readme added.

---
+++ README.md
@@ -0,0 +1,57 @@
+# CarParkingAPIs
+
+#### Simple carparking management APIs builtin NodeJS using Express Framework and MongoDB as database
+
+## Installation
+
+1. Change termial directory to where _package.json_ file is located and Run following command
+
+    ```sh
+    npm install
+    ```
+
+2. To start the server
+
+    ```sh
+    npm start
+    ```
+
+3. To start the server in dev mode
+
+    ```sh
+    npm run dev
+    ```
+
+4. To start the server in testing mode
+
+    ```sh
+    npm start
+    ```
+
+## API points
+
+### 1. First Task API end point is POST on "/parkCar"
+
+This end point excepts a Car number as input and if there is any Parking Slot available the allot that Slot to this car.
+
+Input format (`carNo`)
+    
+
+### 2. Second Task API end point is POST on "/unparkCar"
+
+This end point excepts a Slot number as input and if any car is parked on that slot then remove that car and make that slot available for other cars.
+
+Input format (`slotNo`)
+
+### 3. Third Task API end point is GET on "/"
+
+This end point excepts either Slot number or Car number and returns a both Car Number and Slot Number both for the input.
+
+Input format (`carNo/slotNo`)
+
+## .ENV
+
+.env file contains two variables
+
+1. ParkingCapacity
+2. MongoURI :- I am give my MongoDB Atlas UIR for assesment, if you want you can put your MongoDB URI here.
--- routes/carParkingRoutes.js
+++ routes/carParkingRoutes.js
@@ -5,99 +5,112 @@ const router = express.Router();
 const CarPark = require("../models/carParkingSchema");
 
 //1 - Park a Car
-router.post("/parkCar", async (req, res) => {
-	const ParkingSlot = await CarPark.findOne({ slotAvailable: true });
-	if (!req.body.carNo || req.body.carNo == "") {
-		res.status(412).send("Please enter the car no. with the field");
-	} else {
-		if (ParkingSlot && req.body.carNo) {
-			ParkingSlot.carNo = req.body.carNo;
-			ParkingSlot.slotAvailable = false;
-			ParkingSlot.save()
-				.then(res.status(200).send(ParkingSlot.slotNo))
-				.catch((err) => {
-					console.log(err);
-					res.status(503).send("Data couldn't be sent to database successfully");
-				});
-		} else {
-			await CarPark.find().countDocuments((err, count) => {
-				if (err) {
-					res.status(503).send("Database Error");
-				} else {
-					if (count < process.env.ParkingCapacity) {
-						
-						const newSlot = new CarPark();
 
-						newSlot.slotNo = count + 1;
-						newSlot.carNo = req.body.carNo;
-						newSlot.slotAvailable = false;
-						newSlot
-							.save()
-							.then(res.status(200).send(newSlot.slotNo))
-							.catch((err) => {
-								console.log(err);
-								res.status(503).send("Data couldn't be sent to database successfully");
-							});
+router.all("/parkCar", async (req, res) => {
+	console.log(req.method);
+	if (req.method != "POST") {
+		res.status(405).send();
+	} else {
+		const ParkingSlot = await CarPark.findOne({ slotAvailable: true });
+		if (!req.body.carNo || req.body.carNo == "") {
+			res.status(412).send("Please enter the car no. with the field");
+		} else {
+			if (ParkingSlot && req.body.carNo) {
+				ParkingSlot.carNo = req.body.carNo;
+				ParkingSlot.slotAvailable = false;
+				ParkingSlot.save()
+					.then(res.status(200).send(ParkingSlot.slotNo))
+					.catch((err) => {
+						console.log(err);
+						res.status(503).send("Data couldn't be sent to database successfully");
+					});
+			} else {
+				await CarPark.find().countDocuments((err, count) => {
+					if (err) {
+						res.status(503).send("Database Error");
 					} else {
-						res.status(404).send("No Empty slot found!");
+						if (count < process.env.ParkingCapacity) {
+							const newSlot = new CarPark();
+
+							newSlot.slotNo = count + 1;
+							newSlot.carNo = req.body.carNo;
+							newSlot.slotAvailable = false;
+							newSlot
+								.save()
+								.then(res.status(200).send(newSlot.slotNo))
+								.catch((err) => {
+									console.log(err);
+									res.status(503).send("Data couldn't be sent to database successfully");
+								});
+						} else {
+							res.status(404).send("No Empty slot found!");
+						}
 					}
-				}
-			});
+				});
+			}
 		}
 	}
 });
 
 //2 - Unpark the Car
-router.post("/unparkCar", async (req, res) => {
-	if (req.body.slotNo) {
-		await CarPark.findOne({ slotNo: req.body.slotNo }).exec((err, ParkingSlot) => {
-			if (err) {
-				res.status(503).send("Database Error");
-			} else {
-				if (ParkingSlot.slotAvailable == false) {
-					ParkingSlot.slotAvailable = true;
-					ParkingSlot.carNo = "";
-					ParkingSlot.save()
-						.then(res.status(200).send(ParkingSlot.carNo))
-						.catch((err) => res.status(503).send("Data couldn't be sent to database successfully"));
-				} else {
-					res.status(404).send("Car not found!");
-				}
-			}
-		});
+router.all("/unparkCar", async (req, res) => {
+	if (req.method != "POST") {
+		res.status(405).send();
 	} else {
-		res.status(412).send("Please enter the Slot number.");
+		if (req.body.slotNo) {
+			await CarPark.findOne({ slotNo: req.body.slotNo }).exec((err, ParkingSlot) => {
+				if (err) {
+					res.status(503).send("Database Error");
+				} else {
+					if (ParkingSlot.slotAvailable == false) {
+						ParkingSlot.slotAvailable = true;
+						ParkingSlot.carNo = "";
+						ParkingSlot.save()
+							.then(res.status(200).send(ParkingSlot.carNo))
+							.catch((err) => res.status(503).send("Data couldn't be sent to database successfully"));
+					} else {
+						res.status(404).send("Car not found!");
+					}
+				}
+			});
+		} else {
+			res.status(412).send("Please enter the Slot number.");
+		}
 	}
 });
 
 // 3 - Get the Car/Slot Information
-router.get("/", async (req, res) => {
-	if (req.body.carNo) {
-		await CarPark.findOne({ carNo: req.body.carNo }).exec((err, ParkingSlot) => {
-			if (err) {
-				res.status(503).send("Database Error");
-			} else if (ParkingSlot == null) {
-				res.status(404).send("Car Not Found!");
-			} else {
-				res.status(200).send({ carNo: ParkingSlot.carNo, slotNo: ParkingSlot.slotNo });
-			}
-		});
-	} else if (req.body.slotNo) {
-		await CarPark.findOne({ slotNo: req.body.slotNo }).exec((err, ParkingSlot) => {
-			if (err) {
-				res.status(503).send("Database Error");
-			} else if (ParkingSlot == null) {
-				if (parseInt(req.body.slotNo) > process.env.ParkingCapacity) {
-					res.status(404).send("Slot Not Found!");
-				} else {
-					res.status(404).send("No car is parked at Slot Number " + req.body.slotNo);
-				}
-			} else {
-				res.status(200).send({ carNo: ParkingSlot.carNo, slotNo: ParkingSlot.slotNo });
-			}
-		});
+router.all("/", async (req, res) => {
+	if (req.method != "GET") {
+		res.status(405).send();
 	} else {
-		res.status(412).send("Send either Slot number or Car number.");
+		if (req.body.carNo) {
+			await CarPark.findOne({ carNo: req.body.carNo }).exec((err, ParkingSlot) => {
+				if (err) {
+					res.status(503).send("Database Error");
+				} else if (ParkingSlot == null) {
+					res.status(404).send("Car Not Found!");
+				} else {
+					res.status(200).send({ carNo: ParkingSlot.carNo, slotNo: ParkingSlot.slotNo });
+				}
+			});
+		} else if (req.body.slotNo) {
+			await CarPark.findOne({ slotNo: req.body.slotNo }).exec((err, ParkingSlot) => {
+				if (err) {
+					res.status(503).send("Database Error");
+				} else if (ParkingSlot == null) {
+					if (parseInt(req.body.slotNo) > process.env.ParkingCapacity) {
+						res.status(404).send("Slot Not Found!");
+					} else {
+						res.status(404).send("No car is parked at Slot Number " + req.body.slotNo);
+					}
+				} else {
+					res.status(200).send({ carNo: ParkingSlot.carNo, slotNo: ParkingSlot.slotNo });
+				}
+			});
+		} else {
+			res.status(412).send("Send either Slot number or Car number.");
+		}
 	}
 });
 
